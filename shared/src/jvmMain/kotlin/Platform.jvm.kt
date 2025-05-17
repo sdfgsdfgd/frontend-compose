1b43836d84a6be3aac91cff6f287902a740f38a6
sdfgsdfg <kaanixir@gmail.com>
2025-05-17 20:47:42 +1000

    dependency cleanup

--- shared/src/jvmMain/kotlin/Platform.jvm.kt
+++
@@ -1,5 +0,0 @@
-class JVMPlatform: Platform {
-    override val name: String = "Java ${System.getProperty("java.version")} OS Name: ${System.getProperty("os.name")}"
-}
-
-actual fun getPlatform(): Platform = JVMPlatform()
